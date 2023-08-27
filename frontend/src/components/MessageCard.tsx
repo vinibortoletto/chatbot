11345730e1ea4f6415dae24477fcdf5783245e37
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 20:43:52 -0300

    feat: use props for message content

--- frontend/src/components/MessageCard.tsx
+++ frontend/src/components/MessageCard.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 interface IProps {
   sender: 'company' | 'user'
+  message: string
 }
 
-export default function MessageCard({ sender }: IProps) {
+export default function MessageCard({ sender, message }: IProps) {
   const styles = {
     bg: sender === 'company' ? 'bg-slate-200' : 'bg-sky-500 text-white',
     position: sender === 'company' ? 'justify-start' : 'justify-end'
@@ -12,9 +13,8 @@ export default function MessageCard({ sender }: IProps) {
 
   return (
     <div className={`flex ${styles.position}`}>
-      <div className={`w-2/3 rounded-lg p-2 ${styles.bg}`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
-        doloremque.
+      <div className={`max-w-[70%] rounded-lg p-2 ${styles.bg} `}>
+        {message}
       </div>
     </div>
   )
