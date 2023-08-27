d6be620511fc195d834d65526679fd99af8a8100
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 18:41:23 -0300

    feat: app header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import Header from 'components/Header'
 import React from 'react'
+import Routers from './routers/Routers'
 
 export default function App() {
   return (
     <div>
-      <Header />
+      <div className="m-4 mx-auto w-96 rounded-lg shadow-lg">
+        <Header />
+        <Routers />
+      </div>
     </div>
   )
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="flex justify-between bg-sky-700 p-4 text-slate-100">
-      <Link to="/">
-        <h1 className="font-bold uppercase">chat bot</h1>
-      </Link>
+    <header className="w-full rounded-t-lg bg-sky-700">
+      <div className="mx-auto flex max-w-3xl justify-between  p-4 text-slate-100">
+        <Link to="/">
+          <h1 className="font-bold uppercase">chat bot</h1>
+        </Link>
 
-      <nav>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="cursor-pointer hover:underline">New chat</li>
-          </Link>
+        <nav>
+          <ul className="flex gap-4">
+            <Link to="/">
+              <li className="cursor-pointer hover:underline">New chat</li>
+            </Link>
 
-          <Link to="/chat-history">
-            <li className="cursor-pointer hover:underline">Chat history</li>
-          </Link>
-        </ul>
-      </nav>
+            <Link to="/chat-history">
+              <li className="cursor-pointer hover:underline">Chat history</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
