8a653e5e8d0e379a16a1ddb55230e57e3296f927
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 18:40:49 -0300

    feat: chat header component

---
+++ frontend/src/components/ChatHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import profilePicture from '../assets/profile.jpg'
+
+export default function ChatHeader() {
+  return (
+    <section className="grid grid-cols-[1fr,3fr] gap-4 border-b py-6">
+      <div>
+        <img
+          className="h-20 w-20 rounded-full object-cover"
+          src={profilePicture}
+          alt="profile picture of a woman"
+        />
+      </div>
+
+      <div>
+        <h2 className="text-lg">Claire</h2>
+        <h3 className="text-sm text-slate-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+          itaque.
+        </h3>
+      </div>
+    </section>
+  )
+}
