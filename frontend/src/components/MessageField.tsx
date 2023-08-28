48e086f68492a27562ebf96e12e60581fa064c1f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 21:33:38 -0300

    feat: message field component

---
+++ frontend/src/components/MessageField.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { IoMdSend } from 'react-icons/io'
+
+export default function MessageField() {
+  return (
+    <div className="-mx-4 my-2 flex items-center justify-between gap-4 border-t  px-4 py-2">
+      <div className="w-full">
+        <label htmlFor="message-field" className="sr-only">
+          Message
+        </label>
+
+        <input
+          id="message-field"
+          type="text"
+          placeholder="Type your message"
+          className="w-full rounded-full border border-slate-300 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-500"
+        />
+      </div>
+
+      <div>
+        <button type="button" className="flex items-center">
+          <IoMdSend fill="rgb(14 165 233)" size={25} />
+        </button>
+      </div>
+    </div>
+  )
+}
