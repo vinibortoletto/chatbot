385925bf96cd49afd290da9b3cc7539692efeba6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 22:18:56 -0300

    feat: home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import ChatHeader from '../components/ChatHeader'
+import ChatTime from '../components/ChatTime'
+import MessageCard from '../components/MessageCard'
+import MessageField from '../components/MessageField'
 
-const Home = () => {
-  return <div>Home</div>
+export default function Home() {
+  return (
+    <div className="rounded-b-lg">
+      <ChatHeader />
+
+      <div className="flex h-[22.4rem] flex-col gap-1 overflow-y-scroll p-4">
+        <ChatTime />
+
+        <MessageCard
+          sender="company"
+          message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, earum."
+        />
+        <MessageCard sender="user" message="Lorem ipsum dolor sit amet." />
+        <MessageCard
+          sender="company"
+          message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur perferendis, at iure quos vero pariatur."
+        />
+        <MessageCard sender="user" message="Lorem, ipsum dolor." />
+        <MessageCard sender="user" message="Lorem, ipsum dolor." />
+        <MessageCard sender="user" message="Lorem, ipsum dolor." />
+        <MessageCard sender="user" message="Lorem, ipsum dolor." />
+        <MessageCard sender="user" message="Lorem, ipsum dolor." />
+      </div>
+
+      <MessageField />
+    </div>
+  )
 }
-
-export default Home
