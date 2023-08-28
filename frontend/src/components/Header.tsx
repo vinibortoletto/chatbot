cdd934667489bf46abb7172b8371b160f8699613
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 22:19:42 -0300

    style: header text color

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <header className="w-full rounded-t-lg bg-sky-700">
-      <div className="mx-auto flex max-w-3xl justify-between  p-4 text-slate-100">
+      <div className="mx-auto flex max-w-3xl justify-between  p-4 text-white">
         <Link to="/">
           <h1 className="font-bold uppercase">chat bot</h1>
         </Link>
@@ -12,7 +12,7 @@ export default function Header() {
         <nav>
           <ul className="flex gap-4">
             <Link to="/">
-              <li className="cursor-pointer hover:underline">New chat</li>
+              <li className="cursor-pointer hover:underline">Home</li>
             </Link>
 
             <Link to="/chat-history">
