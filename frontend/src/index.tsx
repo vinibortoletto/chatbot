caa384917e13c207d23f24cbc3ee01d0db7e2bbf
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-27 22:04:50 -0300

    style: custom scroll bar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import './globalStyles.css'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
