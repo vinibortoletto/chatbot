06b07edc5464313cf72f560e45379c10151375ba
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-28 00:17:33 -0300

    feat: create welcome message

--- frontend/src/contexts/MessageContext.tsx
+++ frontend/src/contexts/MessageContext.tsx
@@ -2,11 +2,13 @@ import React, {
   FormEvent,
   createContext,
   useCallback,
+  useEffect,
   useMemo,
   useState
 } from 'react'
 import { IMessage } from '../interfaces'
 import { v4 as uuidv4 } from 'uuid'
+import { handleLocalStorage } from '../utils'
 
 interface IProps {
   children: React.ReactNode
@@ -42,11 +44,14 @@ export function MessageProvider({ children }: IProps) {
         id: uuidv4(),
         content: message,
         createdAt: new Date(),
+        sender: 'user',
         userId: 'user id',
         chatId: 'chat id'
       }
 
-      setMessageList([...messageList, newMessage])
+      const newMessageList = [...messageList, newMessage]
+      setMessageList(newMessageList)
+      handleLocalStorage.set('messageList', newMessageList)
       setMessage('')
     },
     [message, messageList]
@@ -62,6 +67,32 @@ export function MessageProvider({ children }: IProps) {
     }),
     [message, setMessage, messageList, setMessageList, createNewMessage]
   )
+
+  const createWelcomeMessage = useCallback((): void => {
+    const welcomeMessage: IMessage = {
+      id: uuidv4(),
+      content: `Hello, I'm Claire! How can I help you today?`,
+      createdAt: new Date(),
+      sender: 'company',
+      userId: null,
+      chatId: uuidv4()
+    }
+
+    const newMessageList = [...messageList, welcomeMessage]
+    setMessageList(newMessageList)
+    handleLocalStorage.set('messageList', newMessageList)
+  }, [])
+
+  useEffect(() => {
+    const localMessageList = handleLocalStorage.get('messageList') as IMessage[]
+
+    if (localMessageList) {
+      setMessageList(localMessageList)
+      return
+    }
+
+    createWelcomeMessage()
+  }, [])
 
   return (
     <MessageContext.Provider value={value}>{children}</MessageContext.Provider>
