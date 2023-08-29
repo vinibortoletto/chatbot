ac7cca3a3741393331c1c31e09d429ebd9a9c619
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-29 18:02:39 -0300

    feat: after chat ended, disable chat

--- frontend/src/components/MessageField.tsx
+++ frontend/src/components/MessageField.tsx
@@ -3,7 +3,8 @@ import { IoMdSend } from 'react-icons/io'
 import { MessageContext } from '../contexts/MessageContext'
 
 export default function MessageField() {
-  const { message, setMessage, createNewMessage } = useContext(MessageContext)
+  const { message, setMessage, createNewMessage, isChatting } =
+    useContext(MessageContext)
 
   return (
     <form
@@ -19,15 +20,23 @@ export default function MessageField() {
           id="message-field"
           type="text"
           placeholder="Type your message"
-          className="w-full rounded-full border border-slate-300 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-500"
+          className="w-full rounded-full border border-slate-300 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-sky-500 disabled:bg-slate-200"
           value={message}
           onChange={(event) => setMessage(event.target.value)}
+          disabled={!isChatting}
         />
       </div>
 
       <div>
-        <button type="submit" className="flex items-center">
-          <IoMdSend fill="rgb(14 165 233)" size={25} />
+        <button
+          type="submit"
+          className="flex items-center"
+          disabled={!isChatting}
+        >
+          <IoMdSend
+            fill={isChatting ? 'rgb(14 165 233)' : ' rgb(148 163 184)'}
+            size={25}
+          />
         </button>
       </div>
     </form>
