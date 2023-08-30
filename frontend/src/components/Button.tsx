56f83fca554e5ea4a307f5fef35adeafc3cea2d9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-30 08:56:57 -0300

    feat: onClick function to button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 interface IProps {
   text: string
+  onClick?: () => void
 }
 
-export default function Button({ text }: IProps) {
+export default function Button({ text, onClick }: IProps) {
   return (
     <button
       type="button"
       className="rounded-lg border border-sky-500 p-2 text-sky-500 transition hover:bg-sky-500/10"
+      onClick={onClick}
     >
       {text}
     </button>
