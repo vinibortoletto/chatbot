981e552814fa5f247cee7ac0249fdf9d11a78374
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-30 09:05:31 -0300

    style: app height

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <MessageProvider>
       <div className="relative h-[100vh]">
-        <div className="absolute left-1/2 top-1/2 w-full max-w-[24rem] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white shadow-lg">
+        <div className="absolute left-1/2 top-1/2 h-[96vh] w-full max-w-[24rem] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white shadow-lg">
           <Header />
           <Routers />
         </div>
