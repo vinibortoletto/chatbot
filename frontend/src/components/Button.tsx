1516efd9383a398e76436c1714f1e5beca5f892a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-08-30 09:36:40 -0300

    feat: add classes to button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 interface IProps {
   text: string
   onClick?: () => void
+  className?: string
 }
 
-export default function Button({ text, onClick }: IProps) {
+export default function Button({ text, onClick, className }: IProps) {
   return (
     <button
       type="button"
-      className="rounded-lg border border-sky-500 p-2 text-sky-500 transition hover:bg-sky-500/10"
+      className={`rounded-lg border border-sky-500 p-2 text-sky-500 transition hover:bg-sky-500/10 ${className}`}
       onClick={onClick}
     >
       {text}
